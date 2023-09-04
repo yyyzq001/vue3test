d68f8f7d51df3107efc29d40ccf2a521af47c7e0
yexiyu <yexiyu@etrump.net>
2023-09-04 23:01:02 +0800

    23.9.4----音视频测试

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,6 +1,14 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <video width="640" height="360" controls>
+      <source src="../assets/test.mp4" />
+    </video>
+    <audio controls>
+      <!-- <source src="horse.ogg" /> -->
+      <source src="../assets/test.mp3" />
+      您的浏览器不支持 audio 元素。
+    </audio>
   </div>
 </template>
 
